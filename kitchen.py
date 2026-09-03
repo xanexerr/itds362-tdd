8d256746ba77204e0c71bbeda4ef3dce2bdfa0c3
Natthapumin Klammat <natthapumin.klammat@gmail.com>
2026-09-03 12:17:46 +0700

    A6-3 : oz to g

--- kitchen.py
+++ kitchen.py
@@ -1,30 +1,49 @@
 class Quantity:
-    def __init__(self, amount, unit):
+     def __init__(self, amount, unit="g"):
         self.amount = amount
         self.unit = unit
 
-    def times(self, multiplier):
+     def times(self, multiplier):
         return Quantity(self.amount * multiplier, self.unit)
 
-    def plus(self, other):
+     def plus(self, other):
         return Sum(self, other)
 
-    def __eq__(self, other):
+     def __eq__(self, other):
         return (
             self.amount == other.amount
             and self.unit == other.unit
         )
+     def reduce(self, unit):
+          if self.unit == unit:
+               return Quantity(self.amount, unit)
 
-    def __repr__(self):
+          if self.unit == "oz" and unit == "g":
+               return Quantity(self.amount * 28.35, "g")
+
+          if self.unit == "g" and unit == "oz":
+               return Quantity(self.amount / 28.35, "oz")
+
+          raise ValueError(f"แปลงจาก {self.unit} เป็น {unit} ไม่ได้")
+   
+     def __repr__(self):
         return f"Quantity({self.amount}, {self.unit!r})"
 
 
 class Sum:
-    def __init__(self, left, right):
+     def __init__(self, left, right):
         self.left = left
         self.right = right
 
+     def reduce(self, unit):
+        left = self.left.reduce(unit)
+        right = self.right.reduce(unit)
 
+        return Quantity(
+            left.amount + right.amount,
+            unit
+        )
+        
 class Converter:
     def reduce(self, expression, unit):
-        return expression
+        return expression.reduce(unit)
--- test_kitchen.py
+++ test_kitchen.py
@@ -2,19 +2,24 @@ from kitchen import *
 
 
 
-# def test_multiplication():
-#      assert Quantity(200).times(3) == Quantity(600)
-#      assert Quantity(200).times(2) == Quantity(400)
-#      assert grams(1) != ounces(1)
+def test_multiplication():
+     assert Quantity(200).times(3) == Quantity(600)
+     assert Quantity(200).times(2) == Quantity(400)
+     assert grams(1) != ounces(1)
      
-# def test_simple_addition():
-#     total = grams(200).plus(grams(300))
-#     converter = Converter()
-#     assert converter.reduce(total, "g") == grams(500)
+def test_simple_addition():
+    total = grams(200).plus(grams(300))
+    converter = Converter()
+    assert converter.reduce(total, "g") == grams(500)
     
 def test_plus_returns_sum():
     total = grams(200).plus(grams(300))
     assert isinstance(total, Sum)
+    
+def test_addition_with_ounces():
+    total = grams(200).plus(ounces(1))
+    converter = Converter()
+    assert converter.reduce(total, "g") == grams(228.35)
     
 def grams(amount):
     return Quantity(amount, "g")
